6f5f2b31e2acb177737031416221e9d77536fbbe
Lakshmipatnam <105779623+Lakshmipatnam@users.noreply.github.com>
2022-05-18 18:35:38 +0530

    Create factorial.c

---
+++ factorial.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main()
+{
+    int n , number;
+    printf("find the factorial fo this number");
+    scanf("%d",&number);
+    factorial=n(n-1)!;
+    printf("factorail of given number is =%d",factorial);
+    return 0;
+}
